650724f3ae0e631d3c0c09ede48756c8e658e798
Ingvar Derešivski <inde999@gmail.com>
2022-02-02 21:47:35 +0200

    WeatherViewModel

---
+++ Targv20Shop/Targv20Shop.ApplicationServices/Services/WeatherForecastServices.cs
@@ -0,0 +1,24 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targv20Shop.ApplicationServices.Services
+{
+    public class WeatherForecastServices
+    {
+        public async Task<WeatherDto> WeatherResponse()
+        {
+            string idWeather = "tijodrFSC6VO2AQIyEAHAPl2tAwnbpI5";
+            var Location = "asd";
+            //connection string
+            var client = new RestClient($"http://dataservice.accuweather.com/forecasts/v1/daily/1day/{Location}?apikey={idWeather}");
+            var client1 = new RestClient($"http://dataservice.accuweather.com/forecasts/v1/daily/1day/127964?apikey=tijodrFSC6VO2AQIyEAHAPl2tAwnbpI5");
+
+
+            return null;
+        }
+    }
+}
